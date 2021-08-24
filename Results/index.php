79304544f46b7671212e8a6d3de957b73557abb4
unknown <creativ.drops@gmail.com>
2021-08-24 16:31:35 +0000

    ACACIAQUIZAPP

--- Results/index.php
+++ Results/index.php
@@ -226,7 +226,30 @@ if(isset($_GET['code']))
         </div>
     </div>
    </section> 
+   <button onclick="topFunction()" id="myBtn" title="Go to top">☝️</button>
+   <!-- <a href="#top" id="back-to-top" title="Back to top" class="show"><i class="fa fa-arrow-circle-o-up" style="font-size:22px;"></i></a> -->
    <script src="CookieService.js"></script>
    <script src="popup.js"></script>
+   <script>
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+</script>
 </body>
 </html>
